feb41509af5d61dadb7248fedb4ca7ffd66ff3ad
js20028 <mix2088@nate.com>
2022-09-27 16:31:17 +0900

    페이지구성

---
+++ TradingRecordApp/MemoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  MemoViewController.swift
+//  TradingRecordApp
+//
+//  Created by 곽재선 on 2022/09/27.
+//
+
+import UIKit
+
+class MemoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
