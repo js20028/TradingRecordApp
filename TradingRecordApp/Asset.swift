b1ea5ccf42001f62314c8d31f7fe64e3b2101e85
js20028 <mix2088@nate.com>
2022-10-26 16:09:50 +0900

    자산추가 페이지 UI, 자산 모델 추가

---
+++ TradingRecordApp/Asset.swift
@@ -0,0 +1,15 @@
+//
+//  Asset.swift
+//  TradingRecordApp
+//
+//  Created by 곽재선 on 2022/10/26.
+//
+
+import Foundation
+
+struct Asset {
+    let category: String
+    let categoryName: String
+    let coinName: String
+    var coinAmount: Double
+}
