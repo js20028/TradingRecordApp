f44c915d08a6a19246ea9254682be60cd57e4503
js20028 <mix2088@nate.com>
2022-12-08 23:48:05 +0900

    팝업 위치조정, 자동포커싱

--- TradingRecordApp/Asset/AddAssetPopUpViewController.swift
+++ TradingRecordApp/Asset/AddAssetPopUpViewController.swift
@@ -28,6 +28,8 @@ class AddAssetPopUpViewController: UIViewController {
         
         self.popupView.layer.cornerRadius = 10
         
+        self.coinNameTextField.becomeFirstResponder()
+        
         
     }
     
